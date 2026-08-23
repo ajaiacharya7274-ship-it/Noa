ddc5ed350b234857cb8826bd739e55b25b56524a
ajaiacharya7274-ship-it <ajaiacharya7274@gmail.com>
2026-08-23 18:54:20 +0530

    Add basePath and assetPrefix to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  basePath: '/Noa',
+  assetPrefix: '/Noa/',
+
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
